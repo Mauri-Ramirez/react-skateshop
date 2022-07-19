b7556f071649a1e22c6eb3ffc8cac95c574095c3
Mauricio Ramirez <maurizio_514@hotmail.com>
2022-07-19 16:51:02 -0300

    cambio json

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
+
 const Item = ({product}) => {
   return (
     <div>
     <div>{product.img}</div>
     <div>{product.nombre}</div>
     <div>{product.precio}</div>
-
     </div>
     
   )
