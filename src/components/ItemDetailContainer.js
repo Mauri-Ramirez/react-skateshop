d50c35adab8769e448fae6e1deaed6200ac97605
Mauricio Ramirez <maurizio_514@hotmail.com>
2022-08-10 02:59:34 -0300

    firebase

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail';
-import {  productsCollection } from "../utils/firebaseConfig";
-import { getDocs, query, where } from "firebase/firestore";
+import {  db } from "../utils/firebaseConfig";
+import {   collection, doc, getDoc } from "firebase/firestore";
+
 
 function ItemDetailContainer () {
 
@@ -10,20 +11,17 @@ function ItemDetailContainer () {
     const {detailId} = useParams();
 
     useEffect(() =>{
-     /*  const myPromise = new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(products);
-        }, 1000);
-      });
 
-      myPromise.then((res)=>{
-        setProductList(res.find((items) => items.id === parseInt(detailId)))
-      })
- */
-     const itemFilter = query(productsCollection, where("id", "==", detailId))
-
-     getDocs(itemFilter)
-     .then(res => setProductList(res.docs.data()))
+     const itemFilter = collection(db, "products");
+     const itemRef = doc(itemFilter, detailId);
+     getDoc(itemRef)
+     .then(res =>{
+      const product = {
+        id: res.id,
+        ...res.data(),
+      }
+      setProductList(product);
+     })
      .catch(err => console.log(err))
 
     }, [detailId]);
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ItemList from './ItemList';
 import { useParams } from 'react-router-dom';
-import { productsCollection } from '../utils/firebaseConfig';
-import { where, query, getDocs } from "firebase/firestore";
+import { db } from '../utils/firebaseConfig';
+import { getDocs, collection} from "firebase/firestore";
 
 
 
@@ -15,13 +15,19 @@ function ItemListContainer() {
 
     useEffect (() => {
 
-      const requesFilter = id ? query(productsCollection, where("categoria", "==", id)) : productsCollection
-
-      getDocs(requesFilter)
-      .then(result => setProductList(result.docs.map(doc => doc.data())))
-
+      const firestoreFetch = async () => {
+        const productFilter = await getDocs(collection(db, "products"))
+        const dataFromFirestore = productFilter.docs.map((doc) =>({
+          id: doc.id,
+          ...doc.data()
+          
+        }))
+        return dataFromFirestore
+      }
+      firestoreFetch()
+      .then(result => setProductList(result))
       .catch(err => console.log(err))
-      
+
     }, [id]); 
 
     return (
